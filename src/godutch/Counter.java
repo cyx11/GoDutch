f7e3e833a648bd3297dfdca668ed51f44d2cf2ef
Yuxuan Chen <chenyanchao6@126.com>
2017-04-11 17:30:35 -0400

    modify Counter

--- src/godutch/Counter.java
+++ src/godutch/Counter.java
@@ -8,20 +8,15 @@ public class Counter {
 	
 	class Diff {
 		public String p_name;
-		public double diff;
+		public double val;
 		
-		public Diff (String p_name, double diff) {
+		public Diff (String p_name, double val) {
 			this.p_name = p_name;
-			this.diff = diff;
+			this.val = val;
 		}
 	}
 	
 	public String countTransfers(List<Person> persons) {
-		double total = 0;
-		for (Person person : persons) {
-			total += person.getTotalPayment();
-		}
-		double avg = total / persons.size();
 		
 		persons.sort(new Comparator<Person>() {
 			public int compare (Person p1, Person p2) {
@@ -35,15 +30,34 @@ public class Counter {
 			}
 		});
 		
+		double total = 0;
+		for (Person person : persons) {
+			total += person.getTotalPayment();
+		}
+		double avg = total / persons.size();
 		List<Diff> diffs = new ArrayList<Diff>();
 		for (Person person : persons) {
 			diffs.add(new Diff(person.getName(), person.getTotalPayment() - avg));
 		}
 		
 		int start = 0, end = diffs.size() - 1;
+		StringBuilder sb = new StringBuilder();
 		while (start < end) {
-			
+			Diff diffStart = diffs.get(start);
+			Diff diffEnd = diffs.get(end);
+			if (Math.abs(diffStart.val) < Math.abs(diffEnd.val)) {
+				sb.append(diffStart.p_name + "->" + diffEnd.p_name + " " + Math.abs(diffStart.val));
+				diffEnd.val -= Math.abs(diffStart.val);
+				start++;
+			} else if (Math.abs(diffStart.val) > Math.abs(diffEnd.val)) {
+				sb.append(diffStart.p_name + "->" + diffEnd.p_name + " " + Math.abs(diffEnd.val));
+				diffStart.val += Math.abs(diffEnd.val);
+				end--;
+			} else {
+				start++; end--;
+			}
 		}
+		return sb.toString();
 	}
 
 }
